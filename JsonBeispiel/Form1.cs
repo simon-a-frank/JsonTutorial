3ae0d994000dde29cf6b737a02ae16392cf01203
simon-a-frank <simon.a.frank@gmail.com>
2016-12-15 14:04:22 +0100

    Kommentare aktualisiert

--- JsonBeispiel/Form1.cs
+++ JsonBeispiel/Form1.cs
@@ -10,6 +10,17 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+/*
+
+    Codebeispiel zu folgendem Tutorial:
+
+    http://frank-it-beratung.com/blog/2016/12/07/tutorial-json-und-c-teil-1-video/
+
+    YouTube Video: https://youtu.be/SVN4bKO8dTk
+
+    */
+
 namespace JsonBeispiel
 {
     public partial class Form1 : Form
@@ -21,7 +32,7 @@ namespace JsonBeispiel
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //WebClient.DownloadData(....
+            //WebClient.DownloadData(...)
             string json = textBox1.Text;
             byte[] daten = Encoding.UTF8.GetBytes(json);
             MemoryStream ms = new MemoryStream(daten);
